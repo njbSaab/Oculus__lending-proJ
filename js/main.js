0609315292e17db8bbfddfc8e9751a6f69990524
Pro <Admin@Pro.local>
2023-02-23 22:12:09 +0200

    final

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-$(function(){
+'use strict'
 
-});
+const tabItem = document.querySelectorAll('.tabs-btn__item')
+const tabContant = document.querySelectorAll('.tabs-content-item')
+
+tabItem.forEach(elem => elem.addEventListener('click', open));
+
+function open(event){
+   const tabTarget = event.currentTarget;
+   const button = tabTarget.dataset.btn
+   
+   tabItem.forEach(item => item.classList.remove('tabs-btn__item--active'))
+   tabContant.forEach(item => item.classList.remove('tabs-content-item--active'))
+
+   tabTarget.classList.add('tabs-btn__item--active')
+   document.querySelector(`#${button}`).classList.add('tabs-content-item--active')
+}
